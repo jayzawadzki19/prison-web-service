d86c1e0f569d1930ed55b95b108d7dc777796d86
Jay <jzawadzki19@gmail.com>
2021-06-01 22:54:58 +0200

    backend init

---
+++ prison-web-service-backend/src/main/java/pl/ipp31/prisonwebservicebackend/PrisonWebServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.ipp31.prisonwebservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonWebServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrisonWebServiceBackendApplication.class, args);
+    }
+
+}
---
+++ prison-web-service-backend/src/test/java/pl/ipp31/prisonwebservicebackend/PrisonWebServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ipp31.prisonwebservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonWebServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
