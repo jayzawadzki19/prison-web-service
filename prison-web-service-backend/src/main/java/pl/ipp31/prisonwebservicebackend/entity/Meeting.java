db76cef676c37d447b05306a595b5fb8f20dc392
Jay <jzawadzki19@gmail.com>
2021-06-16 20:24:42 +0200

    meetings func init

--- prison-web-service-backend/src/main/java/pl/ipp31/prisonwebservicebackend/entity/Meeting.java
+++ prison-web-service-backend/src/main/java/pl/ipp31/prisonwebservicebackend/entity/Meeting.java
@@ -1,5 +1,6 @@
 package pl.ipp31.prisonwebservicebackend.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Data
 @Table(name = "meetings")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Meeting {
 
     @Id
