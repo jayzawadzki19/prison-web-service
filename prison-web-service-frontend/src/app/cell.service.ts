f4dcc36a6da8491b0cc6dda6a09e2f6ed4193f5b
Karo <noisses.00@gmail.com>
2021-06-23 10:30:42 +0200

    Added cell component and service

---
+++ prison-web-service-frontend/src/app/cell.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Cell} from './cell/cell.component';
+import { Observable } from 'rxjs';
+import { threadId } from 'worker_threads';
+@Injectable({
+  providedIn: 'root'
+})
+export class CellService {
+
+  constructor(private http:HttpClient) { }
+  getCells():Observable<Cell[]>{
+    return this.http.get<Cell[]>('https://localhost:8080/cells/getAll');
+
+  }
+  getCell(id:number):Observable<Cell>{
+    return this.http.get<Cell>('https://localhost:8080/cells/byId'+id);
+  }
+  getCellByCellNumber(cellNumber:number){
+    return this.http.get<Cell>('https://localhost:8080/cells/byCellNumber'+cellNumber);
+  }
+  getCellBySpots(spots:number){
+    return this.http.get<Cell>('https://localhost:8080/cells/bySpots'+spots);
+  }
+  addCell(cell:Cell):Observable<Cell>{
+    return this.http.post<Cell>('https://localhost:8080/cells/new',cell);
+  }
+ // deletecell(cell:Cell):Observable<Cell>{
+  //  return ; 
+ // }
+
+}
